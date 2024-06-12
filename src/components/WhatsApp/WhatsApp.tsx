b3365fddba7485fee71f04b31e65bd2ffc55ee93
unknown <2003silvagui@gmail.com>
2024-06-12 15:09:24 -0300

    feat: redirect

--- src/components/WhatsApp/WhatsApp.tsx
+++ src/components/WhatsApp/WhatsApp.tsx
@@ -5,7 +5,7 @@ export default function WhatsApp() {
   return (
     <>
       <Link
-        href={"https://wa.me/" + process.env.NEXT_PUBLIC_WHATSAPP}
+        href={"https://wa.me/77999577372"}
         target="_blank"
         className="bg-green-500 w-16 h-16 rounded-full fixed right-5 bottom-5 flex items-center justify-center"
       >
